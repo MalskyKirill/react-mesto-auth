776d5b66a3d6eb0e1dc05130faa78875dfebe179
Kirill Malskiy <sabbat@mail.ru>
2023-07-17 07:38:01 +0300

    реализовал выезжающее бургер меню для мобильной версии

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,55 @@
 import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
 
 function Header({ loggedIn, userEmail, handleSingOut }) {
+  const [burger, setBurger] = useState(true);
+
   const location = useLocation();
 
   const title = location.pathname === '/sign-up' ? 'Вход' : 'Регистрация';
   const path = location.pathname === '/sign-up' ? '/sign-in' : '/sign-up';
 
   return (
-    <header className='header'>
-      <div className='header__logo'></div>
-      {loggedIn ? (
-        <div className='header__wrap'>
+    <>
+      <ul className={burger ? 'burger' : 'burger burger_active'}>
+        <li>
           <p className='header__email'>{userEmail}</p>
+        </li>
+        <li>
           <button onClick={handleSingOut} className='header__link-in'>
             Выйти
           </button>
-        </div>
-      ) : (
-        <Link className='header__link' to={path}>
-          {title}
-        </Link>
-      )}
-    </header>
+        </li>
+      </ul>
+      <header className='header'>
+        <div className='header__logo'></div>
+        {loggedIn ? (
+          <>
+            <div className='header__wrap'>
+              <p className='header__email'>{userEmail}</p>
+              <button onClick={handleSingOut} className='header__link-in'>
+                Выйти
+              </button>
+            </div>
+            <div
+              onClick={() => setBurger(!burger)}
+              className='header__mobile-btn'
+            >
+              {burger ? (
+                <AiOutlineMenu size={25} />
+              ) : (
+                <AiOutlineClose size={25} />
+              )}
+            </div>
+          </>
+        ) : (
+          <Link className='header__link' to={path}>
+            {title}
+          </Link>
+        )}
+      </header>
+    </>
   );
 }
 
